9b0f4b2d6f2a497f3738c2bfc726d88531d15a24
manuel-ios <manuel.ios.catalan@gmail.com>
2023-06-28 12:21:57 -0600

    Login

---
+++ script.js
@@ -0,0 +1,9 @@
+// let a = prompt("Dame un numero")
+let nombre
+nombre = prompt("Ingresa tu nombre")
+
+if(nombre != null){
+    document.write("Bienvanido "+nombre)
+}else{
+    alert("No has introduccido. Recarga la página para intentarlo")
+}
